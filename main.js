6ebf07707108c3e460f037d0673d730134a7a2cd
ViannaRosa <99560016+ViannaRosa@users.noreply.github.com>
2022-02-12 17:11:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,105 @@
+/*  toggle menu  */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/*swiper*/
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  Keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+/*ScrollReveal*/
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+  #home .image, #home .text,
+  #about .image, #about .text,
+  #services header, .container .card,
+  #testimonials .title, #testimonials .testimonials,
+  #contact .text, #contact .links, footer .brand, footer .social
+`,
+  { intervel: 100 }
+)
+
+/* scroll header */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+}
+
+/* Arrow Up */
+const arrowUpBottun = document.querySelector('.arrow')
+function arrowUp() {
+  if (window.scrollY >= 560) {
+    arrowUpBottun.classList.add('show')
+  } else {
+    arrowUpBottun.classList.remove('show')
+  }
+}
+
+/* section hold hover */
+const sections = document.querySelectorAll('main section[id]')
+function sectionHoldHover() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  arrowUp()
+  sectionHoldHover()
+})
